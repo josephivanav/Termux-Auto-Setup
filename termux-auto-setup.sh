94b40b409ac8724e08e707404c48fecad3572b3c
Philip Joseph <philipjosephsm@protonmail.com>
2023-03-05 18:53:37 +0600

    Add files via upload

---
+++ termux-auto-setup.sh
@@ -0,0 +1,38 @@
+#! /data/data/com.termux/files/usr/bin/bash
+#---Termux Auto Setup By Philip Joseph-------
+echo "" #blank
+echo "" #blank
+#logo of this script
+figlet "Termux Auto Setup"
+echo "Creator: Philip Joseph"
+echo "" #blank
+
+#Required Packages List
+apt install python -y
+apt install python2 -y
+apt install php -y
+apt install perl -y
+apt install c -y
+apt install c++ -y
+apt install git -y
+apt install nano -y
+apt install vim -y
+apt install wget -y
+apt install curl -y
+apt install zip -y
+apt install unzip -y
+apt install tar -y
+apt install rar -y
+apt install unrar -y
+apt install less -y
+apt install cowsay -y
+apt install ruby -y
+apt install cmatrix -y
+apt install nmap -y
+
+#end
+echo "####################################################################"
+echo "--------------------------------------------------------------------"
+echo "--------Installation is Completed--------"
+echo "--------------------------------------------------------------------"
+echo "###################################################################"
